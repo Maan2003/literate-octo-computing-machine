da48b44c8ff33a777df2464117824ddafa4903e2
Maan2003 <manmeetmann2003@gmail.com>
2023-03-19 08:41:00 +0530

    even more changes

--- db.sh
+++ db.sh
@@ -6,6 +6,7 @@ if test -z "$SSH_KEY"; then
   echo "Warning: SSH_KEY not set, skipping SSH setup"
 else
   echo "$SSH_KEY" >> /home/gitpod/.ssh/authorized_keys
+fi
 
 [ -f /workspace/dropbear.hostkey ] || dropbearkey -t rsa -f /workspace/dropbear.hostkey
 dropbear -r /workspace/dropbear.hostkey -F -E -s -p 2222 -P ~/dropbear.pid
